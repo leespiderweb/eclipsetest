24f5ede0256b99b16ea778459864f434d8e3b4ff
spider.lee <leespiderweb@gmail.com>
2017-04-07 16:45:51 +0800

    第一次提交代码。

---
+++ src/main/java/web/spiderlee/Git_Maven_test/App.java
@@ -0,0 +1,13 @@
+package web.spiderlee.Git_Maven_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
